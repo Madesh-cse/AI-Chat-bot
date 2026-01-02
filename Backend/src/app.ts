dae6eac8e454ac81efd41ba2411c33f3f6850e32
Madesh <madeshdex10@gmail.com>
2026-01-02 19:11:16 +0530

    update the root folder in server side

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -5,12 +5,17 @@ import express from "express";
 // npm i --save-dev @types/morgan
 import morgon from 'morgan'
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 import appRouter from "./routes";
 
 const app = express();
 
 // middleware
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRETE_KEY))
 
