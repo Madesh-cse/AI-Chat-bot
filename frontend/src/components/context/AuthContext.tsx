279cf564996413054f790bb3840e7f1b49247385
Madesh <madeshdex10@gmail.com>
2026-01-02 19:17:46 +0530

    initialize the auth context for state management for authentication

--- frontend/src/components/context/AuthContext.tsx
+++ frontend/src/components/context/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState, type ReactNode } from "react";
 import { createContext } from "react";
+import { checkAuthStatus, LoginUser } from "../../helper/api-communicators";
 
 type User = {
     name: string;
@@ -23,11 +24,29 @@ export const AuthProvider = ({children}: {children: ReactNode})=>{
 
     useEffect(()=>{
         // fetch the user cookies  are valid then skip login
+        async function checkStatus(){
+            const data = await checkAuthStatus();
+            if(data){
+                setuser({
+                    email: data.email,
+                    name: data.name
+                })
+                setisLoggedIn(true)
+            }
+        }
+        checkStatus()
     },[])
 
     // we creating the methods
     const login = async(email: string, password: string)=>{
-
+        const data = await LoginUser(email, password);
+        if(data){
+            setuser({
+                email: data.email,
+                name: data.name
+            });
+            setisLoggedIn(true);
+        }
     }
     const signup = async(name: string,email: string, password: string)=>{
 
